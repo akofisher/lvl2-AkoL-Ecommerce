512f59ebf1b9fdd53478f3b7b594d6146cc6dd48
akofisher <akolekveishvili@gmail.com>
2021-07-07 17:15:45 +0400

    NEWNEW

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import React from 'react'
 import './App.scss'
-import { makeStyles } from '@material-ui/core/styles'
-import { Grid, Box } from '@material-ui/core'
-import Header from './Header/Header'
-import Body from './Body/Body'
-import Social from './Footer/Social'
-import Informations from './Footer/Informations'
-
-const useStyles = makeStyles((theme) => ({
-  grid: {
-    width: '100%',
-    margin: '0px',
-  },
-  box: {
-    padding: theme.spacing(0),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    background: theme.palette.success.light,
-  },
-}))
+import Loyout from './Loyout/Loyout'
 
 function App() {
-  const classes = useStyles()
-
   return (
-    <Grid container className={classes.grid}>
-      <Header />
-
-      <Body />
-
-      <Grid item xs={12}>
-        <Social />
-        <Informations />
-      </Grid>
-    </Grid>
+    <React.Fragment>
+      <Loyout />
+    </React.Fragment>
   )
 }
 
--- src/Body/RightSide/FetchStore.js
+++ src/Body/RightSide/FetchStore.js
@@ -5,44 +5,210 @@ import CardActionArea from '@material-ui/core/CardActionArea'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
-import { useState, useEffect } from 'react'
+import { Grid } from '@material-ui/core'
+// import { useState, useEffect } from 'react'
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
+const useStyles = makeStyles((theme) => ({
   media: {
     height: 140,
   },
-})
+  flexible: {
+    display: 'flex',
+    padding: '60px 34px',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+  },
+  padd: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+  },
+}))
 
 export default function FakeStore() {
-  const [products, setProducts] = useState([])
+  // const [products, setProducts] = useState([])
   const classes = useStyles()
 
-  useEffect(() => {
-    fetch('https://fakestoreapi.com/products?limit=9')
-      .then((res) => res.json())
-      .then((data) => setProducts(data))
-  }, [])
+  // useEffect(() => {
+  //   fetch('https://fakestoreapi.com/products?limit=9')
+  //     .then((res) => res.json())
+  //     .then((data) => setProducts(data))
+  // }, [])
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
-          title="title"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            title
-          </Typography>
-          <Typography gutterBottom component="h2">
-            price
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <React.Fragment>
+      <Grid container className={classes.flexible}>
+        <Grid md={4}>
+          <Card className={classes.padd}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
+                title="title"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  მაისური
+                </Typography>
+                <Typography gutterBottom component="h2">
+                  0.50$
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid md={4}>
+          <Card className={classes.padd}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
+                title="title"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  მაისური
+                </Typography>
+                <Typography gutterBottom component="h2">
+                  0.50$
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid md={4}>
+          <Card className={classes.padd}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
+                title="title"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  მაისური
+                </Typography>
+                <Typography gutterBottom component="h2">
+                  0.50$
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid md={4}>
+          <Card className={classes.padd}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
+                title="title"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  მაისური
+                </Typography>
+                <Typography gutterBottom component="h2">
+                  0.50$
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid md={4}>
+          <Card className={classes.padd}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
+                title="title"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  მაისური
+                </Typography>
+                <Typography gutterBottom component="h2">
+                  0.50$
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid md={4}>
+          <Card className={classes.padd}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
+                title="title"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  მაისური
+                </Typography>
+                <Typography gutterBottom component="h2">
+                  0.50$
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid md={4}>
+          <Card className={classes.padd}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
+                title="title"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  მაისური
+                </Typography>
+                <Typography gutterBottom component="h2">
+                  0.50$
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid md={4}>
+          <Card className={classes.padd}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
+                title="title"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  მაისური
+                </Typography>
+                <Typography gutterBottom component="h2">
+                  0.50$
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid md={4}>
+          <Card className={classes.padd}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
+                title="title"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  მაისური
+                </Typography>
+                <Typography gutterBottom component="h2">
+                  0.50$
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
+    </React.Fragment>
   )
 }
