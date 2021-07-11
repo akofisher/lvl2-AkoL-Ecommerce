6f0f0f6c9a9d2d062f019deb33abcd5ca54de0d4
akofisher <akolekveishvili@gmail.com>
2021-07-11 16:48:23 +0400

    NEWNEW

--- src/Body/RightSide/components/FetchStore.js
+++ src/Body/RightSide/components/FetchStore.js
@@ -6,7 +6,7 @@ import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 import { Grid } from '@material-ui/core'
-// import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 const useStyles = makeStyles((theme) => ({
   media: {
@@ -25,189 +25,90 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 export default function FakeStore() {
-  // const [products, setProducts] = useState([])
   const classes = useStyles()
-
-  // useEffect(() => {
-  //   fetch('https://fakestoreapi.com/products?limit=9')
-  //     .then((res) => res.json())
-  //     .then((data) => setProducts(data))
-  // }, [])
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      img: 'https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg',
+      title: 'მაისური',
+      price: '0.50$',
+    },
+    {
+      id: 2,
+      img: 'https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg',
+      title: 'მაისური',
+      price: '0.60$',
+    },
+    {
+      id: 3,
+      img: 'https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg',
+      title: 'მაისური',
+      price: '0.70$',
+    },
+    {
+      id: 4,
+      img: 'https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg',
+      title: 'მაისური',
+      price: '0.80$',
+    },
+    {
+      id: 5,
+      img: 'https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg',
+      title: 'მაისური',
+      price: '0.90$',
+    },
+    {
+      id: 6,
+      img: 'https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg',
+      title: 'მაისური',
+      price: '1$',
+    },
+    {
+      id: 7,
+      img: 'https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg',
+      title: 'მაისური',
+      price: '2$',
+    },
+    {
+      id: 8,
+      img: 'https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg',
+      title: 'მაისური',
+      price: '3$',
+    },
+    {
+      id: 9,
+      img: 'https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg',
+      title: 'მაისური',
+      price: '4$',
+    },
+  ])
 
   return (
     <React.Fragment>
       <Grid container className={classes.flexible}>
-        <Grid xs={12} md={4}>
-          <Card className={classes.padd}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
-                title="title"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  მაისური
-                </Typography>
-                <Typography gutterBottom component="h2">
-                  0.50$
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid xs={12} md={4}>
-          <Card className={classes.padd}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
-                title="title"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  მაისური
-                </Typography>
-                <Typography gutterBottom component="h2">
-                  0.50$
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid xs={12} md={4}>
-          <Card className={classes.padd}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
-                title="title"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  მაისური
-                </Typography>
-                <Typography gutterBottom component="h2">
-                  0.50$
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid xs={12} md={4}>
-          <Card className={classes.padd}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
-                title="title"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  მაისური
-                </Typography>
-                <Typography gutterBottom component="h2">
-                  0.50$
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid xs={12} md={4}>
-          <Card className={classes.padd}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
-                title="title"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  მაისური
-                </Typography>
-                <Typography gutterBottom component="h2">
-                  0.50$
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid xs={12} md={4}>
-          <Card className={classes.padd}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
-                title="title"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  მაისური
-                </Typography>
-                <Typography gutterBottom component="h2">
-                  0.50$
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid xs={12} md={4}>
-          <Card className={classes.padd}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
-                title="title"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  მაისური
-                </Typography>
-                <Typography gutterBottom component="h2">
-                  0.50$
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid xs={12} md={4}>
-          <Card className={classes.padd}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
-                title="title"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  მაისური
-                </Typography>
-                <Typography gutterBottom component="h2">
-                  0.50$
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid xs={12} md={4}>
-          <Card className={classes.padd}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="https://sportmaster.ge/wp-content/uploads/2019/04/3-35.jpg"
-                title="title"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  მაისური
-                </Typography>
-                <Typography gutterBottom component="h2">
-                  0.50$
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
+        {products.map((data) => {
+          return (
+            <Grid xs={12} md={4} key={data.id}>
+              <Card className={classes.padd}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={data.img}
+                    title={data.title}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {data.title}
+                    </Typography>
+                    <Typography gutterBottom component="h2">
+                      {data.price}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          )
+        })}
       </Grid>
     </React.Fragment>
   )
